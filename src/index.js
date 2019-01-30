bf0caf3981c8e102a9e273034f739525b13bc26f
linkel <ironcarbide@gmail.com>
2019-01-29 19:57:55 -0800

    Wrapped App in Redux Provider

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import {applyMiddleware, createStore} from 'redux';
+import reduxThunk from 'redux-thunk';
+//import logger from 'redux-logger';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(reduxThunk));
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
